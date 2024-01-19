5d8b01a7480b451cf80f0800cb5e1b5840d87fd4
spiritualbimbo <isabellajames.hottie@gmail.com>
2024-01-19 17:16:26 +0100

    This is a app that list your daily schedule

--- script.js
+++ script.js
@@ -1,82 +1,69 @@
-const inputField = document.querySelector(".input-field");
-const five = document.querySelector(".five-perc");
-const ten = document.querySelector(".ten-perc");
-const fifteen = document.querySelector(".fifteen-perc");
-const twenty = document.querySelector(".twenty-perc");
-const twentyFive = document.querySelector(".twentyfive-perc");
-const fifty = document.querySelector(".fifty-perc");
-const custom = document.querySelector(".custom");
-const numberOfPeople = document.querySelector(".num-of-people");
-const amount = document.querySelector(".totalTip");
-const totalPerson = document.querySelector(".totalBill");
-const reset = document.querySelector(".btn");
-const people = document.querySelector(".people");
-const warning = document.querySelector(".warning");
-const init = function () {
-  inputField.value = 0;
-  amount.textContent = "0.00";
-  totalPerson.textContent = "0.00";
-  people.value = 0;
-  custom.value = '';
-  reset.setAttribute('disabled', '');
-  warning.textContent = "";
-  people.style.border = "none";
-  people.removeAttribute('disabled')
+window.addEventListener('load', () => {
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector('#new-task-input');
+  const list_el = document.querySelector('#tasks');
 
-};
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-reset.addEventListener("click", function () {
-  init();
-});
+    const task = input.value;
 
-window.addEventListener("load", function () {
-  init();
-});
+    if(!task) {
+      alert("please fill out the task");
+      return;
+    } 
 
-five.addEventListener("click", function () {
-  calculate(5);
-});
+    const task_el = document.createElement("div");
+     task_el.classList.add("task");
 
-ten.addEventListener("click", function () {
-  calculate(10);
-});
+    const task_content_el = document.createElement("div");
+    task_content_el.classList.add("content");
+    // task_content_el.innerText = task;
 
-fifteen.addEventListener("click", function () {
-  calculate(15);
-});
+    task_el.appendChild(task_content_el);
 
-twentyFive.addEventListener("click", function () {
-  calculate(25);
-});
+    const task_input_el = document.createElement("input");
+    task_input_el.classList.add("text");
+    task_input_el.type = "text";
+    task_input_el.value = task;
+    task_input_el.setAttribute("readonly", "readonly");
 
-fifty.addEventListener("click", function () {
-  calculate(50);
-});
+    task_content_el.appendChild(task_input_el); 
 
-custom.addEventListener("change", function () {
-  calculate(custom.value);
-});
+    const task_actions_el = document.createElement("div");
+    task_actions_el.classList.add ("actions");
 
-const calculate = function (tip) {
-  const bill = inputField.value;
- 
-   if(people.value > 0){
-    const tipPerPerson = parseInt((tip/100) * bill);
-    amount.textContent = tipPerPerson;
-    const totPerson = (parseInt(bill)/parseInt(people.value) + parseInt(tipPerPerson));
-    totalPerson.textContent = totPerson;
-   }else{
-    warning.textContent = "can't be zero";
-    reset.removeAttribute('disabled');
-    people.style.border = "3px solid red";
-    people.style.borderRadius = "5px";
-    people.setAttribute('disabled', "")
-   }
-   if(parseInt(amount.textContent) > 0 || parseInt(totalPerson.textContent) > 0){
-    reset.removeAttribute('disabled');
-   }
+    const task_edit_el = document.createElement("button");
+    task_edit_el.classList.add("edit");
+    task_edit_el.innerHTML = "Edit";
+
+
+    const task_delete_el = document.createElement("button");
+    task_delete_el.classList.add("delete");
+    task_delete_el.innerHTML = "Delete";
+
+    task_actions_el.appendChild (task_edit_el);
+    task_actions_el.appendChild (task_delete_el);
+
+    task_el.appendChild (task_actions_el);
+
+    list_el.appendChild(task_el);
+
+    input.value ="";
+
+    task_edit_el.addEventListener('click', () => {
+    if(task_edit_el.innerText.toLowerCase() == "edit"){
+      task_input_el.removeAttribute("readonly");
+      task_input_el.focus();
+      task_edit_el.innerText = "Save";
+    } else {
+      task_input_el.setAttribute("readonly", "readonly");
+      task_edit_el.innerHTML = "Edit";
+    }
+    });
   
-};
-
-
-
+    task_delete_el.addEventListener('click', () => {
+      list_el.removeChild(task_el);
+    });
+  });
+})
